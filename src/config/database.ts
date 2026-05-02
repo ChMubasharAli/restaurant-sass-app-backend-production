63e73b966872d2636ed757e17dd836e9b6655fc4
ChMubasharAli <muhammadmubashirali075@gmail.com>
2026-05-02 11:52:04 +0500

    update changes

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { config } from "./index.js";
-import { PrismaClient } from "../../generated/prisma/client.js";
+import { PrismaClient } from "../generated/prisma/client.js";
 
 const connectionString = `${config.database.url}`;
 const adapter = new PrismaPg({ connectionString });
